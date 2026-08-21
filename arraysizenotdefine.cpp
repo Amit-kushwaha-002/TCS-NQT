1a78d09195571bbdbd9f39e0552bd5dd46071aa0
Amit <amkit01golu@gmail.com>
2026-08-21 08:44:55 +0530

    Add ARRAY folder and solutions cleanly

---
+++ arraysizenotdefine.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+  string s;
+  getline(cin , s);
+  stringstream ss(s);
+  vector<int>arr;
+  int num;
+  while(ss>>num){
+    arr.push_back(num);
+  }
+  for (int a:arr)
+  {
+    cout<<a <<"";
+  }
+  
+}
